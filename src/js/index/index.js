c829618034012379d8b6a37821033039744357b1
Jascha Ehrenreich <jascha@jaeh.at>
2015-12-04 11:00:47 +0100

    font-off button is self-mounting now

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -1,5 +1,17 @@
-let btnFontToggle = document.getElementById('font-toggle');
+let d = document;
+let b = d.body;
+let fontBtn = d.createElement('button');
+let fClass = 'font-off';
+fontBtn.id = 'font-toggle';
 
-btnFontToggle.addEventListener('click', (evt) => {
-  document.body.classList.toggle('font-off');
+fontBtn.addEventListener('click', evt => {
+  if (b.className.indexOf(fClass) > -1) {
+    b.className.replace(fClass, '');
+  } else {
+    b.className = b.className + ' ' + fClass;
+  }
+
+  b.classList.toggle(fClass);
 });
+
+b.appendChild(fontBtn);
